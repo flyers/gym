c7079fee3052af6f9da6bfd0f16d20c41d9ea7e8
flyers <lisiyi.zju@gmail.com>
2019-02-27 20:01:16 +0800

    add several reward functions

--- gym/envs/vrep/vrep_env.py
+++ gym/envs/vrep/vrep_env.py
@@ -84,7 +84,6 @@ class VREPEnv(VREPBaseEnv):
                                self.target_coordinates.tolist(),
                                dtype='float32')
         else:
-            a = 0
             return numpy.array(self.linear_velocity_b.tolist() +
                                self.angular_velocity_b +
                                [self.quadcopter_pos[2], ] +
@@ -106,58 +105,280 @@ class VREPEnv(VREPBaseEnv):
 
     def eval_reward_0(self, action):
         if self._game_over():
-            reward = -10.0
-        else:
-            r_coord = goal_func_3(
-                self.target_coordinates[:2] - self._goal_target[:2], [0.1, 0.25])
-            r_scale = goal_func_3(
-                self.target_coordinates[2] - self._goal_target[2], [0.05, 0.2])
-            r_orientation = aux_func(numpy.array(
-                self.quadcopter_orientation[0:2]) / numpy.pi - 0, 0.03) * 1.0
-            r_height = aux_func(
-                self.quadcopter_pos[2] - self._goal_height, 0.5) * 0.5
-            reward = r_coord + r_scale + r_orientation + r_height
-            if self._log:
-                logger.info(
-                    'TargetLoc_x Reward:%.4f\nTargetLoc_y Reward:%.4f\nTargetLoc_h Reward:%.4f\nTargetPos Reward:%.4f\nW Reward:%f\nTotal Reward:%.4f\n' %
-                    (
-                        r_coord,
-                        r_coord,
-                        r_scale,
-                        r_height,
-                        r_orientation,
-                        reward,
-                    )
+            return -10.0
+        r_coord = goal_func_3(
+            self.target_coordinates[:2] - self._goal_target[:2], [0.1, 0.25])
+        r_scale = goal_func_3(
+            self.target_coordinates[2] - self._goal_target[2], [0.05, 0.2])
+        r_orientation = aux_func(numpy.array(
+            self.quadcopter_orientation[0:2]) / numpy.pi - 0, 0.03) * 1.0
+        r_height = aux_func(
+            self.quadcopter_pos[2] - self._goal_height, 0.5) * 0.5
+        reward = r_coord + r_scale + r_orientation + r_height
+        if self._log:
+            logger.info(
+                'TargetLoc_x Reward:%.4f\nTargetLoc_y Reward:%.4f\nTargetLoc_h Reward:%.4f\nTargetPos Reward:%.4f\nW Reward:%f\nTotal Reward:%.4f\n' %
+                (
+                    r_coord,
+                    r_coord,
+                    r_scale,
+                    r_height,
+                    r_orientation,
+                    reward,
                 )
-
+            )
         return reward
 
     def eval_reward_1(self, action):
+        """
+        Add penalty for large actions 
+        """
         if self._game_over():
-            reward = -10.0
-        else:
-            r_coord = goal_func_3(
-                self.target_coordinates[:2] - self._goal_target[:2], [0.1, 0.25])
-            r_scale = goal_func_3(
-                self.target_coordinates[2] - self._goal_target[2], [0.05, 0.2])
-            r_orientation = aux_func(numpy.array(
-                self.quadcopter_orientation[0:2]) / numpy.pi - 0, 0.03) * 1.0
-            r_height = aux_func(
-                self.quadcopter_pos[2] - self._goal_height, 0.5) * 0.5
-            reward = r_coord + r_scale + r_orientation + r_height
-            if self._log:
-                logger.info(
-                    'TargetLoc_x Reward:%.4f\nTargetLoc_y Reward:%.4f\nTargetLoc_h Reward:%.4f\nTargetPos Reward:%.4f\nW Reward:%f\nTotal Reward:%.4f\n' %
-                    (
-                        r_coord,
-                        r_coord,
-                        r_scale,
-                        r_height,
-                        r_orientation,
-                        reward,
-                    )
+            return -10.0
+        r_coord = goal_func_3(
+            self.target_coordinates[:2] - self._goal_target[:2], [0.1, 0.25])
+        r_scale = goal_func_3(
+            self.target_coordinates[2] - self._goal_target[2], [0.05, 0.2])
+        r_orientation = aux_func(numpy.array(
+            self.quadcopter_orientation[0:2]) / numpy.pi - 0, 0.03) * 1.0
+        r_height = aux_func(
+            self.quadcopter_pos[2] - self._goal_height, 0.5) * 0.5
+        r_action = aux_func(action, 0.5)
+        reward = r_coord + r_scale + r_orientation + r_height + r_action
+        if self._log:
+            logger.info(
+                'TargetLoc_x Reward:%.4f\nTargetLoc_y Reward:%.4f\nTargetLoc_h Reward:%.4f\nTargetPos Reward:%.4f\nW Reward:%f\nTotal Reward:%.4f\n' %
+                (
+                    r_coord,
+                    r_coord,
+                    r_scale,
+                    r_height,
+                    r_orientation,
+                    reward,
                 )
+            )
+        return reward
 
+    def eval_reward_2(self, action):
+        """
+        Add penalty for large actions 
+        """
+        if self._game_over():
+            return -10.0
+        r_coord = goal_func_3(
+            self.target_coordinates[:2] - self._goal_target[:2], [0.1, 0.25])
+        r_scale = goal_func_3(
+            self.target_coordinates[2] - self._goal_target[2], [0.05, 0.2])
+        r_orientation = aux_func(numpy.array(
+            self.quadcopter_orientation[0:2]) / numpy.pi - 0, 0.03) * 1.0
+        r_height = aux_func(
+            self.quadcopter_pos[2] - self._goal_height, 0.5) * 0.5
+        r_action = aux_func(action, 0.3)
+        reward = r_coord + r_scale + r_orientation + r_height + r_action
+        if self._log:
+            logger.info(
+                'TargetLoc_x Reward:%.4f\nTargetLoc_y Reward:%.4f\nTargetLoc_h Reward:%.4f\nTargetPos Reward:%.4f\nW Reward:%f\nTotal Reward:%.4f\n' %
+                (
+                    r_coord,
+                    r_coord,
+                    r_scale,
+                    r_height,
+                    r_orientation,
+                    reward,
+                )
+            )
+        return reward
+
+    def eval_reward_3(self, action):
+        """
+        Add penalty for large actions 
+        """
+        if self._game_over():
+            return -10.0
+        r_coord = goal_func_3(
+            self.target_coordinates[:2] - self._goal_target[:2], [0.1, 0.25])
+        r_scale = goal_func_3(
+            self.target_coordinates[2] - self._goal_target[2], [0.05, 0.2])
+        r_orientation = aux_func(numpy.array(
+            self.quadcopter_orientation[0:2]) / numpy.pi - 0, 0.03) * 1.0
+        r_height = aux_func(
+            self.quadcopter_pos[2] - self._goal_height, 0.5) * 0.5
+        r_action = aux_func(action, 0.1)
+        reward = r_coord + r_scale + r_orientation + r_height + r_action
+        if self._log:
+            logger.info(
+                'TargetLoc_x Reward:%.4f\nTargetLoc_y Reward:%.4f\nTargetLoc_h Reward:%.4f\nTargetPos Reward:%.4f\nW Reward:%f\nTotal Reward:%.4f\n' %
+                (
+                    r_coord,
+                    r_coord,
+                    r_scale,
+                    r_height,
+                    r_orientation,
+                    reward,
+                )
+            )
+        return reward
+
+    def eval_reward_4(self, action):
+        """
+        Add penalty for large action variations 
+        """
+        if self._game_over():
+            return -10.0
+        r_coord = goal_func_3(
+            self.target_coordinates[:2] - self._goal_target[:2], [0.1, 0.25])
+        r_scale = goal_func_3(
+            self.target_coordinates[2] - self._goal_target[2], [0.05, 0.2])
+        r_orientation = aux_func(numpy.array(
+            self.quadcopter_orientation[0:2]) / numpy.pi - 0, 0.03) * 1.0
+        r_height = aux_func(
+            self.quadcopter_pos[2] - self._goal_height, 0.5) * 0.5
+        past_actions = numpy.array(self.actions)
+        delta_actions = past_actions[1::] - past_actions[0:-1]
+        r_action = aux_func(delta_actions, 0.5) * 0.5
+        reward = r_coord + r_scale + r_orientation + r_height + r_action
+        if self._log:
+            logger.info(
+                'TargetLoc_x Reward:%.4f\nTargetLoc_y Reward:%.4f\nTargetLoc_h Reward:%.4f\nTargetPos Reward:%.4f\nW Reward:%f\nTotal Reward:%.4f\n' %
+                (
+                    r_coord,
+                    r_coord,
+                    r_scale,
+                    r_height,
+                    r_orientation,
+                    reward,
+                )
+            )
+        return reward
+
+    def eval_reward_5(self, action):
+        """
+        Add penalty for large action variations 
+        """
+        if self._game_over():
+            return -10.0
+        r_coord = goal_func_3(
+            self.target_coordinates[:2] - self._goal_target[:2], [0.1, 0.25])
+        r_scale = goal_func_3(
+            self.target_coordinates[2] - self._goal_target[2], [0.05, 0.2])
+        r_orientation = aux_func(numpy.array(
+            self.quadcopter_orientation[0:2]) / numpy.pi - 0, 0.03) * 1.0
+        r_height = aux_func(
+            self.quadcopter_pos[2] - self._goal_height, 0.5) * 0.5
+        past_actions = numpy.array(self.actions)
+        delta_actions = past_actions[1::] - past_actions[0:-1]
+        r_action = aux_func(delta_actions, 0.3) * 0.5
+        reward = r_coord + r_scale + r_orientation + r_height + r_action
+        if self._log:
+            logger.info(
+                'TargetLoc_x Reward:%.4f\nTargetLoc_y Reward:%.4f\nTargetLoc_h Reward:%.4f\nTargetPos Reward:%.4f\nW Reward:%f\nTotal Reward:%.4f\n' %
+                (
+                    r_coord,
+                    r_coord,
+                    r_scale,
+                    r_height,
+                    r_orientation,
+                    reward,
+                )
+            )
+        return reward
+
+    def eval_reward_6(self, action):
+        """
+        Add penalty for large action variations 
+        """
+        if self._game_over():
+            return -10.0
+        r_coord = goal_func_3(
+            self.target_coordinates[:2] - self._goal_target[:2], [0.1, 0.25])
+        r_scale = goal_func_3(
+            self.target_coordinates[2] - self._goal_target[2], [0.05, 0.2])
+        r_orientation = aux_func(numpy.array(
+            self.quadcopter_orientation[0:2]) / numpy.pi - 0, 0.03) * 1.0
+        r_height = aux_func(
+            self.quadcopter_pos[2] - self._goal_height, 0.5) * 0.5
+        past_actions = numpy.array(self.actions)
+        delta_actions = past_actions[1::] - past_actions[0:-1]
+        r_action = aux_func(delta_actions, 0.1) * 0.5
+        reward = r_coord + r_scale + r_orientation + r_height + r_action
+        if self._log:
+            logger.info(
+                'TargetLoc_x Reward:%.4f\nTargetLoc_y Reward:%.4f\nTargetLoc_h Reward:%.4f\nTargetPos Reward:%.4f\nW Reward:%f\nTotal Reward:%.4f\n' %
+                (
+                    r_coord,
+                    r_coord,
+                    r_scale,
+                    r_height,
+                    r_orientation,
+                    reward,
+                )
+            )
+        return reward
+
+    def eval_reward_7(self, action):
+        """
+        Add penalty for large action accelerations
+        """
+        if self._game_over():
+            return -10.0
+        r_coord = goal_func_3(
+            self.target_coordinates[:2] - self._goal_target[:2], [0.1, 0.25])
+        r_scale = goal_func_3(
+            self.target_coordinates[2] - self._goal_target[2], [0.05, 0.2])
+        r_orientation = aux_func(numpy.array(
+            self.quadcopter_orientation[0:2]) / numpy.pi - 0, 0.03) * 1.0
+        r_height = aux_func(
+            self.quadcopter_pos[2] - self._goal_height, 0.5) * 0.5
+        past_actions = numpy.array(self.actions)
+        delta_actions = past_actions[1::] - past_actions[0:-1]
+        acce_actions = delta_actions[1::] - delta_actions[0:-1]
+        r_action = aux_func(acce_actions, 0.1) * 0.5
+        reward = r_coord + r_scale + r_orientation + r_height + r_action
+        if self._log:
+            logger.info(
+                'TargetLoc_x Reward:%.4f\nTargetLoc_y Reward:%.4f\nTargetLoc_h Reward:%.4f\nTargetPos Reward:%.4f\nW Reward:%f\nTotal Reward:%.4f\n' %
+                (
+                    r_coord,
+                    r_coord,
+                    r_scale,
+                    r_height,
+                    r_orientation,
+                    reward,
+                )
+            )
+        return reward
+
+    def eval_reward_8(self, action):
+        """
+        Add penalty for large action accelerations
+        """
+        if self._game_over():
+            return -10.0
+        r_coord = goal_func_3(
+            self.target_coordinates[:2] - self._goal_target[:2], [0.1, 0.25])
+        r_scale = goal_func_3(
+            self.target_coordinates[2] - self._goal_target[2], [0.05, 0.2])
+        r_orientation = aux_func(numpy.array(
+            self.quadcopter_orientation[0:2]) / numpy.pi - 0, 0.03) * 1.0
+        r_height = aux_func(
+            self.quadcopter_pos[2] - self._goal_height, 0.5) * 0.5
+        r_action = aux_func(action, 0.1)        
+        past_actions = numpy.array(self.actions)
+        delta_actions = past_actions[1::] - past_actions[0:-1]
+        r_action_delta = aux_func(delta_actions, 0.1) * 0.5
+        reward = r_coord + r_scale + r_orientation + r_height + r_action + r_action_delta
+        if self._log:
+            logger.info(
+                'TargetLoc_x Reward:%.4f\nTargetLoc_y Reward:%.4f\nTargetLoc_h Reward:%.4f\nTargetPos Reward:%.4f\nW Reward:%f\nTotal Reward:%.4f\n' %
+                (
+                    r_coord,
+                    r_coord,
+                    r_scale,
+                    r_height,
+                    r_orientation,
+                    reward,
+                )
+            )
         return reward
 
     def _game_over(self):
